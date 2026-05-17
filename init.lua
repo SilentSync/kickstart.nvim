549b5370e3912b0994dba4339154a23e3edd0cf4
silentsync <silentsync@pm.me>
2026-05-17 22:59:17 +0200

    added vimtex plugin

--- init.lua
+++ init.lua
@@ -288,7 +288,16 @@ require('lazy').setup({
       },
     },
   },
-
+--Latex plugin (code from Github) 
+  {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = "zathura"
+    end
+  },
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
